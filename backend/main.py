abf1fa6130dc73cf43b834f260ff0c62bde5fcac
ezpedersen <erikzhangpedersen@gmail.com>
2025-04-26 09:54:27 -0700

    add screen ui positioning route on backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-
+from positioning import generate
 app = FastAPI()
 
 
 @app.post("/parse")
 async def parse_url(url_input: str):
     return {"url": url_input}
+
+
+@app.post("/screen")
+async def screenshot(image: bytes):
+    response = generate(image, "the file button")
+    stripped = "{" + response.text.split("{")[1].split("}")[0] + "}"
+    return stripped
 
 if __name__ == "__main__":
     import uvicorn
